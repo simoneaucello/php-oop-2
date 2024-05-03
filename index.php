c9ae49ff3d377aac30ebaaf60c15206013dc5dbe
simoneaucello <simoneaucello@gmail.com>
2024-05-03 14:42:14 +0200

    [init] repository

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once __DIR__ . '/data/db.php'
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <!-- bootstrap  -->
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css' integrity='sha512-jnSuA4Ss2PkkikSOLtYs8BlYIeeIK1h99ty4YfvRPAlzr377vr3CXDb7sb7eEEBYjDtcYj+AjBH3FLv5uSJuXg==' crossorigin='anonymous' />
+
+  <link rel="stylesheet" href="style.css">
+
+  <title>Pet-shop</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
